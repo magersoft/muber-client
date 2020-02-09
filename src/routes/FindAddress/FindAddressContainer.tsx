044ea47d4b831503eb0619a8177e4ce01f95feea
v.mager <v.mager@b2b-center.ru>
2020-02-10 01:42:45 +0300

    Changed google maps to Yandex.Maps

--- src/routes/FindAddress/FindAddressContainer.tsx
+++ src/routes/FindAddress/FindAddressContainer.tsx
@@ -1,62 +1,62 @@
-import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import Helmet from 'react-helmet';
+import { YMaps, Map } from 'react-yandex-maps';
 import style from './FindAddress.module.scss';
 
-interface IState {
-  lat: number
-  lng: number
+interface IMaps {
+  center: number[];
+  zoom: number
 }
 
 const FindAddress: FunctionComponent<any> = (props) => {
-  let map: google.maps.Map;
-  const mapRef: any = useRef();
-
-  const [state, setState] = useState<IState>({
-    lat: 0,
-    lng: 0
+  const [state, setState] = useState<IMaps>({
+    center: [0, 0],
+    zoom: 14
   });
+  const [map, setMap] = useState<any>({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
-  });
+  }, [navigator.geolocation]);
 
   const handleGeoSuccess = (position: Position) => {
     const { coords: { latitude, longitude } } = position;
     setState({
-      lat: latitude,
-      lng: longitude
+      ...state,
+      center: [latitude, longitude]
     });
-    loadMap(latitude, longitude)
   };
 
   const handleGeoError = () => {
     return;
   };
 
-  const loadMap = (lat, lng) => {
-    const { google } = props;
-    const maps = google.maps;
-    const mapNode = ReactDOM.findDOMNode(mapRef.current);
-    const mapConfig: google.maps.MapOptions = {
-      zoom: 14,
-      center: {
-        lat,
-        lng
-      },
-      disableDefaultUI: true
-    };
-    map = new maps.Map(mapNode, mapConfig);
+  const onBoundsChange = () => {
+    setState({ ...state, center: map.getCenter() });
+    console.log(state.center);
   };
 
   return (
-    <div>
+    <div className={style.Map}>
       <Helmet>
         <title>Find Address | Muber</title>
       </Helmet>
-      <div className={style.Pin}>📍</div>
-      <div className={style.Map} ref={mapRef} />
-    </div>
+        <YMaps
+          query={{
+            lang: 'en_US',
+            apikey: '963d0e32-6ea5-47a6-80f1-9ab4f6571ca3'
+          }}
+        >
+          <div className={style.Pin}>📍</div>
+          <Map
+            state={state}
+            instanceRef={map => setMap(map)}
+            onBoundsChange={onBoundsChange}
+            width={'100%'}
+            height={'100%'}
+          />
+        </YMaps>
+      </div>
   )
 };
 
--- src/routes/FindAddress/index.tsx
+++ src/routes/FindAddress/index.tsx
@@ -1,7 +1,4 @@
-import { GoogleApiWrapper } from 'google-maps-react';
 import FindAddress from './FindAddressContainer';
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDEU87eGMt8RKBf8_6fkq5T7PhB4tablAQ'
-})(FindAddress);
+export default FindAddress;
 
 
