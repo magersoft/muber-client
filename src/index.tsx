7b65da8de3bf703f3eb07c85cda8038030e2c7c4
v.mager <v.mager@b2b-center.ru>
2020-02-17 13:53:50 +0300

    fix serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import client from './apollo';
 import App from './components/App';
 import './index.scss';
+import * as serviceWorker from './serviceWorker';
 
 const app =
 <ApolloProvider client={client}>
@@ -11,3 +12,8 @@ const app =
 </ApolloProvider>;
 
 ReactDOM.render(app, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
